c492832bef0fa965180a9dd8770a9f0380cbea34
virgantara <vinux.edu@gmail.com>
2025-04-26 06:55:06 +0700

    + rel path

--- zoo/CurveNet/core/main_cls.py
+++ zoo/CurveNet/core/main_cls.py
@@ -11,7 +11,10 @@ Modified by
 """
 
 from __future__ import print_function
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
+
 import argparse
 import torch
 import torch.nn as nn
