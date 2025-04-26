c2a8f4ca9fd9674383dd3a0a8a9a0ffc7139f59a
virgantara <vinux.edu@gmail.com>
2025-04-26 07:01:42 +0700

    + rel path

--- zoo/CurveNet/core/main_cls.py
+++ zoo/CurveNet/core/main_cls.py
@@ -53,6 +53,7 @@ def _init_():
     os.system('cp models/curvenet_cls.py ../checkpoints/'+args.exp_name+'/curvenet_cls.py.backup')
 
 def train(args, io):
+    wandb.init(project="UnderCorruption", name=args.exp_name)
     train_loader = DataLoader(ModelNet40(partition='train', num_points=args.num_points), num_workers=8,
                               batch_size=args.batch_size, shuffle=True, drop_last=True)
     test_loader = DataLoader(ModelNet40(partition='test', num_points=args.num_points), num_workers=8,
