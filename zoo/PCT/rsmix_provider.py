aae8fc42db7cfac6869460944775b685a20cb509
virgantara <vinux.edu@gmail.com>
2025-04-27 00:14:13 +0700

    run rpc

--- zoo/PCT/rsmix_provider.py
+++ zoo/PCT/rsmix_provider.py
@@ -121,25 +121,15 @@ def square_distance(src, dst):
     Output:
         dist: per-point square distance, [B, N, M]
     """
-    if isinstance(src, np.ndarray):
-        src = torch.from_numpy(src).float()
-    if isinstance(dst, np.ndarray):
-        dst = torch.from_numpy(dst).float()
-
     B, N, _ = src.shape
     _, M, _ = dst.shape
     # dist = -2 * torch.matmul(src, dst.permute(0, 2, 1))
     # dist += torch.sum(src ** 2, -1).view(B, N, 1)
     # dist += torch.sum(dst ** 2, -1).view(B, 1, M)
 
-    # dist = -2 * np.matmul(src, dst.transpose(0, 2, 1))
-    # dist += np.sum(src ** 2, -1).reshape(B, N, 1)
-    # dist += np.sum(dst ** 2, -1).reshape(B, 1, M)
-
-
-    dist = -2 * torch.matmul(src, dst.transpose(1, 2))   # <-- Fix this
-    dist += torch.sum(src ** 2, -1).view(B, N, 1)
-    dist += torch.sum(dst ** 2, -1).view(B, 1, M)
+    dist = -2 * np.matmul(src, dst.transpose(0, 2, 1))
+    dist += np.sum(src ** 2, -1).reshape(B, N, 1)
+    dist += np.sum(dst ** 2, -1).reshape(B, 1, M)
     
     return dist
 
@@ -161,12 +151,6 @@ def pts_num_ctrl(pts_erase_idx, pts_add_idx):
     return pts_add_idx_ctrled
 
 def rsmix(data_batch, label_batch, beta=1.0, n_sample=512, KNN=False):
-
-    if isinstance(data_batch, np.ndarray):
-        data_batch = torch.from_numpy(data_batch).float()
-    if isinstance(label_batch, np.ndarray):
-        label_batch = torch.from_numpy(label_batch).long()
-        
     cut_rad = np.random.beta(beta, beta)
     rand_index = np.random.choice(data_batch.shape[0],data_batch.shape[0], replace=False) # label dim : (16,) for model
     
