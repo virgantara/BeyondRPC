6ead29b820debf55f0474e32420e2fecde7d610a
virgantara <vinux.edu@gmail.com>
2025-05-19 13:56:39 +0700

    + train on Point Transformer

--- zoo/PointTransformers/model.py
+++ zoo/PointTransformers/model.py
@@ -1,7 +1,29 @@
 import torch
 import torch.nn as nn
-from pointnet_util import farthest_point_sample, index_points, square_distance
+from pointnet_util import index_points, square_distance
 
+def farthest_point_sample(xyz, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [B, N, 3]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [B, npoint]
+    """
+    device = xyz.device
+    B, N, C = xyz.shape
+    centroids = torch.zeros(B, npoint, dtype=torch.long).to(device)
+    distance = torch.ones(B, N).to(device) * 1e10
+    farthest = torch.randint(0, N, (B,), dtype=torch.long).to(device) * 0
+    batch_indices = torch.arange(B, dtype=torch.long).to(device)
+    for i in range(npoint):
+        centroids[:, i] = farthest
+        centroid = xyz[batch_indices, farthest, :].view(B, 1, 3)
+        dist = torch.sum((xyz - centroid) ** 2, -1)
+        mask = dist < distance
+        distance[mask] = dist[mask]
+        farthest = torch.max(distance, -1)[1]
+    return centroids
 
 def sample_and_group(npoint, nsample, xyz, points):
     B, N, C = xyz.shape
